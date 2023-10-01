b8f339b61ee0d179a52ce14dedb602da3e118cc3
Vihaan146 <140971874+Vihaan146@users.noreply.github.com>
2023-10-01 11:16:23 -0700

    Create bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int length){
+    int i,j;
+    for(i=0;i<length-1;i++)
+    {
+        for(j=0;j<length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<length;i++)
+    {
+        printf("%d\t",arr[1]);
+    }
+}
+int main;
